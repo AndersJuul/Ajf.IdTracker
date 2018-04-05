ad9fe4cfe8ebdf434063ad89285be16c2b30d184
AndersJuul <andersjuulsfirma@gmail.com>
2018-04-05 16:50:59 +0200

    a

---
+++ Src/Ajf.IdTracker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ajf.IdTracker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ajf.IdTracker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb5d170a-8821-4cea-8775-2b93842e4f03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Src/Ajf.IdTracker.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace Ajf.IdTracker.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
