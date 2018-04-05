266c656edeab42f447c6e4b7a3d1e2296c928075
AndersJuul <andersjuulsfirma@gmail.com>
2018-04-05 17:50:34 +0200

    a

---
+++ Src/Ajf.IdTracker.Tests/CsvRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Ajf.IdTracker.Shared;
+
+namespace Ajf.IdTracker.Tests
+{
+    [TestFixture]
+    public class CsvRepositoryTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            // Arrange
+            var sut = new CsvRepository();
+
+            // Act
+            var res = sut.GetUniqueNewNumber(new DateTime(2018, 4, 4), "031069-0503");
+
+            // Assert
+            Assert.IsNotNullOrEmpty(res);
+        }
+    }
+}
